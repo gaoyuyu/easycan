55c45cba880a1fe8efbad9d9f275b2ea747cbca4
gaoyuyu <740514999@qq.com>
2016-11-09 00:25:21 +0800

    add Map/get2LocationAndDirection

--- Application/Api/Controller/MapController.class.php
+++ Application/Api/Controller/MapController.class.php
@@ -17,11 +17,12 @@ class MapController extends AppController
      * address=address&key=key
      * ps.address中的空格必须替换成+，否则访问不了api
      */
-    public function getLatAndLngByAddress()
+    public function getLatAndLngByAddress($address)
     {
 //        header("Content-type: text/html; charset=utf-8");
-        $address = $prepAddr = str_replace(' ', '+', $_GET["address"]);
-        $url = "https://maps.googleapis.com/maps/api/geocode/json?address=" . $address . "&key=" . C("GOOGLE_MAPS_API_KEYS");
+//        $address = $prepAddr = str_replace(' ', '+', $_GET["address"]);
+        $address = $this->handleStringForApi($address);
+        $url = "https://maps.googleapis.com/maps/api/geocode/json?language=zh-CN&address={$address}&key=" . C("GOOGLE_MAPS_API_KEYS");
         $json = file_get_contents($url);
         $output = json_decode($json);
         $lat = $output->results[0]->geometry->location->lat;
@@ -32,6 +33,101 @@ class MapController extends AppController
             "formatted_address" => $formattedAddress,
             "location" => $location
         );
-        $this->returnResponseOK($data);
+        return $data;
     }
+
+    /**
+     * 此API返回的实时位置包含饭店位置A，客户位置B，司机当前位置C由Android客户端获取
+     * 返回的Direction包含C->A，A->B
+     * 接收的参数分别为饭店地理位置，客户地理位置
+     */
+    public function get2LocationAndDirection()
+    {
+        $restaurant = trim($_POST["restaurant"]);
+        $customer = trim($_POST["customer"]);
+
+        $localLat = trim($_POST["lat"]);//23.1306821
+        $localLng = trim($_POST["lng"]);//113.3980644
+//        $restaurant = trim($_GET["restaurant"]);
+//        $customer = trim($_GET["customer"]);
+//
+//        $localLat = trim($_GET["lat"]);//23.1306821
+//        $localLng = trim($_GET["lng"]);//113.3980644
+//        $localLat = 23.1306821;//113.3980644
+//        $localLng = 113.3980644;//113.3980644
+
+//        header("Content-type: text/html; charset=utf-8");
+
+        if (empty($restaurant) || empty($customer))
+        {
+            $this->returnResponseError("请检查提交信息是否为空！");
+        }
+
+        $rl = $this->getLatAndLngByAddress($restaurant);
+        $cl = $this->getLatAndLngByAddress($customer);
+
+        //获取从饭店到客户的路径 A-B
+        $restaurant_customer = $this->getDirectionPolyline($rl["formatted_address"], $cl["formatted_address"]);
+        $local = $this->reverseGeocodingForLatLng($localLat, $localLng);
+
+        //获取当前位置到饭店的路径C-A
+        $local_restaurant = $this->getDirectionPolyline($local, $rl["formatted_address"]);
+        $data = array(
+            "restaurant" => $rl,
+            "customer" => $cl,
+            "local"=>$local,
+            "restaurant_customer" => $restaurant_customer,
+            "local_restaurant" => $local_restaurant,
+        );
+
+        $this->returnResponseOK($data);
+
+
+
+
+
+    }
+
+
+    public function handleStringForApi($address)
+    {
+        return str_replace(' ', '+', $address);
+    }
+
+    /**
+     *origin ：起点  destination：终点
+     * https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=YOUR_API_KEY
+     *
+     */
+    public function getDirectionPolyline($orign, $destination)
+    {
+        $orign = $this->handleStringForApi($orign);
+        $destination = $this->handleStringForApi($destination);
+
+        $url = "https://maps.googleapis.com/maps/api/directions/json?language=zh-CN&origin={$orign}&destination={$destination}&key=" . C("GOOGLE_MAPS_API_KEYS");
+
+        $json = file_get_contents($url);
+        $output = json_decode($json);
+
+        $points = $output->routes[0]->overview_polyline->points;
+        return $points;
+    }
+
+
+    /**
+     * 反向地理编码
+     * https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=YOUR_API_KEY
+     */
+    public function reverseGeocodingForLatLng($lat, $lng)
+    {
+        $url = "https://maps.googleapis.com/maps/api/geocode/json?language=zh-CN&latlng={$lat},{$lng}&key=" . C("GOOGLE_MAPS_API_KEYS");
+
+        $json = file_get_contents($url);
+        $output = json_decode($json);
+
+        $formattedAddress = $output->results[0]->formatted_address;
+        return $formattedAddress;
+
+    }
+
 }
